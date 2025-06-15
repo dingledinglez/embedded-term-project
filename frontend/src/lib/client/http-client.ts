b7fa0d1517d491ed78a8fadb8ce1591197044700
성영준 <69851018+yxun20@users.noreply.github.com>
2025-06-15 17:06:25 +0900

    feat: 클라이언트

---
+++ frontend/src/lib/client/http-client.ts
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import useTokenStore from '@/store/TokenStore.ts';
+
+const httpClient = axios.create({
+  baseURL: '/api',
+  headers: {
+    Accept: 'application/json',
+  },
+  beforeRedirect: () => {
+    console.log('Redirecting...');
+  },
+});
+const token = useTokenStore.getState().token;
+httpClient.interceptors.request.use(config => {
+  
+  if (token) {
+    console.log(token);
+    config.headers.Authorization = `Bearer ${token}`;
+    config.withCredentials = true;
+  } else {
+    delete config.headers.Authorization;
+  }
+  return config;
+});
+
+httpClient.interceptors.response.use(
+  response => response,
+  error => {
+    if (error.response?.status === 403) {
+      //alert('403 에러');
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default httpClient;
